e24fa8b9648ebb5998dc34b8142929510a125e1e
ziyu <ziyu@caibaopay.com>
2019-02-17 18:27:21 +0800

    更新简历

--- components/SkillManages.vue
+++ components/SkillManages.vue
@@ -1,29 +1,19 @@
 <template>
   <div class="containers">
     <p class="content">
-      1、熟练掌握JS基础、ES6及以上语法。熟悉 HTML 与 CSS 以及浏览器相关的知识。  
+      1、熟悉前端相关的技术（包括HTML/CSS/JavaScript等），熟悉ES6及以上语法。
     </p>
     <p class="content">
-      2、主要技术栈为 Vue，熟练掌握其开发技巧，了解其实现的原理。React 方面也有相关的项目经验。
+      2、具备跨终端（Mobile+PC）的前端开发能力与桌面（Electron、NW）应用开发的实践经验。  
     </p>
     <p class="content">
-      3、了解设计模式相关的知识，可以编写简洁易维护的代码。
+      3、熟练掌握 使用MVVM（Vue、React）框架开发的技巧，并了解其实现的原理，具备开发大型应用的实践。
     </p>
     <p class="content">
-      4、了解性能优化方面的知识，优化过 H5 动画效果, 从卡顿到流畅。
+      4、熟悉设计模式相关的知识，可以编写简洁易维护的代码。熟悉性能优化方面的知识，有过移动端的动画效果优化的实践。
     </p>
     <p class="content">
-      5、熟悉 http、https 相关的知识
-    </p>
-    <p class="content">
-      6、熟悉Wepack的使用，熟悉其配置各种项以及loader、plugin的使用，编写过自定义的 loader 与 plugin。
-    </p>
-    <p class="content">
-      7、具备客户端应用开发的经验，包括 electron、nw 以及移动端 webapp, 解决过许多兼容性问题。 
-    </p>
-    <p class="content">
-      8、自主学习了 NodeJS， 编写了一个自动化发布发布脚本，为整个团队提升了 5% 的研发效率。
-      并且学习了 koa、 MongoDB 以及服务器部署的相关知识，搭建了一个 <a href="http://todolist.zhoufeifan.tech/dist/index.html#/">todoList</a> 的网站。 
+      5、熟悉网络协议（HTTP/SSL）以及常见安全问题和对策，具备前端工程化与模块化开发的实践经验。
     </p>
   </div>
 </template>
