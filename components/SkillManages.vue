af0a7b90ac4270579b77a99e87a059493c29df57
zhoufeifan <xieyang.zff@raycloud.com>
2019-02-17 13:34:29 +0800

    ..

--- components/SkillManages.vue
+++ components/SkillManages.vue
@@ -4,7 +4,7 @@
       1、熟练掌握JS基础、ES6及以上语法。熟悉 HTML 与 CSS 以及浏览器相关的知识。  
     </p>
     <p class="content">
-      2、主要技术栈为 Vue，熟练掌握其开发技巧，了解其实现的原理，React 方面也有过一段时间的项目经验。
+      2、主要技术栈为 Vue，熟练掌握其开发技巧，了解其实现的原理。React 方面也有相关的项目经验。
     </p>
     <p class="content">
       3、了解设计模式相关的知识，可以编写简洁易维护的代码。
@@ -13,13 +13,16 @@
       4、了解性能优化方面的知识，优化过 H5 动画效果, 从卡顿到流畅。
     </p>
     <p class="content">
-      5、熟悉Wepack的使用，熟悉其配置各种项以及loader、plugin的使用，编写过自定义的 loader 与 plugin。
+      5、熟悉 http、https 相关的知识
     </p>
     <p class="content">
-      6、具备客户端应用开发的经验，包括 electron、nw 以及移动端 webapp, 解决过许多兼容性问题。 
+      6、熟悉Wepack的使用，熟悉其配置各种项以及loader、plugin的使用，编写过自定义的 loader 与 plugin。
     </p>
     <p class="content">
-      7、自主学习了 NodeJS， 编写了一个自动化发布发布脚本，为整个团队提升了 5% 的研发效率。
+      7、具备客户端应用开发的经验，包括 electron、nw 以及移动端 webapp, 解决过许多兼容性问题。 
+    </p>
+    <p class="content">
+      8、自主学习了 NodeJS， 编写了一个自动化发布发布脚本，为整个团队提升了 5% 的研发效率。
       并且学习了 koa、 MongoDB 以及服务器部署的相关知识，搭建了一个 <a href="http://todolist.zhoufeifan.tech/dist/index.html#/">todoList</a> 的网站。 
     </p>
   </div>
