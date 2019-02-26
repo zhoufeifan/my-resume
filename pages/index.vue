a542092787dcb64fb7d98b5df31cbc44f46a70f1
zhoufeifan <xieyang.zff@raycloud.com>
2019-02-26 20:53:14 +0800

    修改简历

--- pages/index.vue
+++ pages/index.vue
@@ -12,16 +12,34 @@
           <div class="draft-group">
             <DraftItem>
               <CareerTitle time="2018.05 — 至今" company="杭州光云科技有限公司" job="前端开发"></CareerTitle>
-              <p class="work-content">
-                1、参与快麦收银软件的开发，涉及到<span class="main-color">Electron、web 与 node 原生模块</span>相关的技术，并改进了PC设备与外接设备之间的通信方式，提升通信的稳定性。<br>
-                2、主要负责『番茄表单』项目C端填表的开发与维护，涉及的技术主要有<span class="main-color">vue 全家桶、微信与钉钉应用相关技术</span>。针对弹出消息提示框这种常见需求的实现方式做了改进，通过<span class="main-color">全局注入插件</span>再进行函数调用的方式，代替了原来声明html元素的方式，提高了开发效率。
-                在移动端环境搭建了用户错误日志收集系统，上报出错的信息（包括调用栈）、设备的型号、网络环境、应用版本等，便于问题的排查。<br>
-                3、单独负责『有成会议移动端报名』项目，用 <span class="main-color">vue-cli3</span> 从 0 到 1 搭建项目，包括组件模块的设计 、<span class="main-color">vue-router</span>的配置、使用<span class="main-color">vuex</span> 模块化管理存储状态等。
-                设计了一个防止网页下拉的组件， 避免在微信、钉钉容器中出现下拉页面的糟糕效果，使得体验更接近于原生。
-                <br>
-                <br>
-                <br>
-              </p>
+              <div class="work-content-list">
+                <div class="work-content">
+                  <p class="title">
+                    快麦收银软件
+                    <span class="skill">Electron、web、node</span>
+                  </p>
+                  <p class="hint">项目描述：是一款用于桌面端的收银软件，主要功能有秤重、会员折扣计价、收款、打印小票等，对接了条码秤、电子秤、打印机、读卡器等多个硬件设备。</p>
+                  <p class="resolve">针对硬件设备与web通信在不同收银机上的兼容性问题，将通信方式从原来的websocket方式改为Electorn原生的进程通信方式，并采用消息节流的方式提升性能，最终解决了这个问题。</p>
+                </div>
+                <div class="work-content">
+                  <p class="title">
+                    番茄表单
+                    <span class="skill">Vue全家桶、微信与钉钉应用</span>
+                  </p>
+                  <p class="hint">项目描述：B端用户在B端创建表单后，生成表单链接，用户通过这个链接打开一个表单，就可以填写相关的信息。主要的应用场景有“问卷调查”、“考试测评”、“投票选举”、“信息收集”等功能。</p>
+                  <p class="resolve">针对运营方面定期产出的需求（首页banner、广告弹窗、公告等）。和同事一起讨论策划方案，搭建了一个后台管理系统，让那些需求通过后台系统配置的方式实现，解决了90%来自运营那边的需求。</p>
+                  <p class="resolve">编写了一个消息提示框插件，通过全局注入到项目中。用一行代码代替原来分散的多行代码实现消息提示框的功能，提升开发效率、降低代码维护成本。</p>
+                  <p class="resolve">针对移动端填表不同设备兼容性问题的排查， 做了报错信息收集上报的功能。通过捕获全局的错误信息，解析堆栈调用，并获取用户的设备型号、网络环境、应用版本等信息进行上报</p>
+                </div>
+                <div class="work-content">
+                  <p class="title">
+                    有成会议移动端报名
+                    <span class="skill">Vue全家桶、CSS3 </span>
+                  </p>
+                  <p class="hint">项目描述：有成会议移动端报名页面，用户通过手机号进行参会人员的添加、查询、删除等，还可以通过微信支付进行报名缴费。</p>
+                  <p class="resolve">针对在微信webview中网页下拉的糟糕体验，设计了一个组件，让页面既可以有效的防止下拉，又可以流畅地进行滚动，实现仿原生的体验。</p>
+                </div>
+              </div>
             </DraftItem>
           </div>
           <div class="draft-group">
